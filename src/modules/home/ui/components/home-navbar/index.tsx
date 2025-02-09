3bfe2ec39d29d6bf2316c4c7dde688d4ad0c4604
narandza <60319154+narandza@users.noreply.github.com>
2025-02-09 10:33:20 +0100

    create home navbar logo

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,3 +1,23 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+
 export const HomeNavbar = () => {
-  return <nav className="">home navbar</nav>;
+  return (
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+      <div className="flex items-center gap-4 w-full">
+        {/* Menu and Logo */}
+        <div className="flex items-center flex-shrink-0">
+          <SidebarTrigger />
+          <Link href="/">
+            {/* TODO: Add constants */}
+            <div className="p-4 flex items-center gap-1">
+              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+              <p className="text-xl font-semibold tracking-tighter">NewTube</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
