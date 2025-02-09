60ac3bce4387acd8db6f0c483709b792425bcd89
narandza <60319154+narandza@users.noreply.github.com>
2025-02-09 11:01:44 +0100

    create main section

--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,9 +1,13 @@
+import { MainSection } from "./main-section";
+
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 
 export const HomeSidebar = () => {
   return (
     <Sidebar className="pt-16 z-40 border-none">
-      <SidebarContent className="bg-background"></SidebarContent>
+      <SidebarContent className="bg-background">
+        <MainSection />
+      </SidebarContent>
     </Sidebar>
   );
 };
