0ed8d7a4170bdb4803f41dfc5594bf8def9fecf9
narandza <60319154+narandza@users.noreply.github.com>
2025-02-10 17:57:21 +0100

    protect route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 // TODO: Enable Email login
 export const config = {
   matcher: [
