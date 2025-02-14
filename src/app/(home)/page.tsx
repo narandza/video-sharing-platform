5cc879e1cb165cdc20d6e29cb426cb2407b0271c
narandza <60319154+narandza@users.noreply.github.com>
2025-02-14 19:29:23 +0100

    add error boundery

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 import { PageClient } from "./client";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
   void trpc.hello.prefetch({ text: "Jova" });
@@ -8,6 +9,7 @@ export default async function Home() {
   return (
     <HydrateClient>
       <Suspense fallback={<p>Loading ...</p>}>
+        <ErrorBoundary fallback={<p>Error</p>}></ErrorBoundary>
         <PageClient />
       </Suspense>
     </HydrateClient>
