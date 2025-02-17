db31a801ab2751f359ac668a817d076d2c9a42d8
narandza <60319154+narandza@users.noreply.github.com>
2025-02-17 17:36:00 +0100

    create categories seed

--- src/scripts/seed-categories.ts
+++ src/scripts/seed-categories.ts
@@ -1,1 +1,40 @@
-// TODO: Create a script to seed categories
+// TODO: Add descriptions
+
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames = [
+  "Cars and vehicles",
+  "Comedy",
+  "Education",
+  "Entertainment",
+  "Film and animation",
+  "How-to and style",
+  "Music",
+  "News and politics",
+  "People and blogs",
+  "Pets and animals",
+  "Science and technology",
+  "Sports",
+  "Travel and events",
+];
+
+async function main() {
+  console.log("Seeding categories...");
+
+  try {
+    const values = categoryNames.map((name) => ({
+      name,
+      description: `Videos related to ${name.toLocaleLowerCase()}`,
+    }));
+
+    db.insert(categories).values(values);
+
+    console.log("Categories seeded successfully.");
+  } catch (error) {
+    console.log("Error seeding categories:", error);
+    process.exit(1);
+  }
+}
+
+main();
