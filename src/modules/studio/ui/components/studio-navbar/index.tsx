6bfacce38f351481d74d21c5b6c13996eb98cd80
narandza <60319154+narandza@users.noreply.github.com>
2025-02-19 19:45:25 +0100

    modify studio navbar

--- src/modules/studio/ui/components/studio-navbar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/index.tsx
@@ -1,31 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { SearchInput } from "./search-input";
-
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { AuthButton } from "@/modules/auth/ui/components/auth-button";
 
 export const StudioNavbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50 border-b shadow-md">
       <div className="flex items-center gap-4 w-full">
         {/* Menu and Logo */}
         <div className="flex items-center flex-shrink-0">
           <SidebarTrigger />
-          <Link href="/">
+          <Link href="/studio">
             {/* TODO: Add constants */}
             <div className="p-4 flex items-center gap-1">
               <Image src="/logo.svg" alt="Logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tighter">NewTube</p>
+              <p className="text-xl font-semibold tracking-tighter">Studio</p>
             </div>
           </Link>
         </div>
 
-        {/* Search bar */}
-        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
-          <SearchInput />
-        </div>
+        {/* Spacer */}
+        <div className="flex-1" />
 
         <div className="flex-shrink-0 items-center flex gap-4">
           <AuthButton />
