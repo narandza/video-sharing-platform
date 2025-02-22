ae53cf128c0dd82bafc1065440828ffa0630c35b
narandza <60319154+narandza@users.noreply.github.com>
2025-02-22 12:11:27 +0100

    prepare prefetch

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,9 +1,17 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 const Page = async () => {
-  void trpc.studio.getMany.prefetchInfinite();
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
 
-  return <HydrateClient>studio</HydrateClient>;
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
--- src/modules/studio/server/procedures.ts
+++ src/modules/studio/server/procedures.ts
@@ -1,11 +1,29 @@
+import { z } from "zod";
+
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, desc, eq, lt, or } from "drizzle-orm";
 
 export const studioRouter = createTRPCRouter({
-  getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(videos);
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
 
-    return data;
-  }),
+      const data = await db.select().from(videos);
+
+      return data;
+    }),
 });
