2ac87cfea02b32a533c4a798f9870f4ece85c476
narandza <60319154+narandza@users.noreply.github.com>
2025-02-22 12:29:19 +0100

    add pending state for create button

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { Loader2Icon, PlusIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 
@@ -8,8 +8,16 @@ export const StudioUploadModal = () => {
   const create = trpc.videos.create.useMutation();
 
   return (
-    <Button variant="secondary" onClick={() => create.mutate()}>
-      <PlusIcon />
+    <Button
+      variant="secondary"
+      onClick={() => create.mutate()}
+      disabled={create.isPending}
+    >
+      {create.isPending ? (
+        <Loader2Icon className="animate-spin" />
+      ) : (
+        <PlusIcon />
+      )}
       Create
     </Button>
   );
