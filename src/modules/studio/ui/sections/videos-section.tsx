d20b56cb8863c1773d054a4b625e9bbee10cfc2c
narandza <60319154+narandza@users.noreply.github.com>
2025-02-23 23:08:21 +0100

    create table header columns

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -5,6 +5,7 @@ import { DEFAULT_LIMIT } from "@/constants";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export const VideosSection = () => {
   return (
@@ -28,6 +29,20 @@ const VideosSectionSuspense = () => {
 
   return (
     <div className="">
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+        </Table>
+      </div>
       {JSON.stringify(data)}
 
       <InfiniteScroll
