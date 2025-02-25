c54e4260581dbcfe3cb436b58539a713be6e6198
narandza <60319154+narandza@users.noreply.github.com>
2025-02-25 04:43:58 +0100

    create format duration util

--- src/modules/videos/ui/components/video-thumnail.tsx
+++ src/modules/videos/ui/components/video-thumnail.tsx
@@ -1,3 +1,4 @@
+import { formatDuration } from "@/lib/utils";
 import Image from "next/image";
 
 interface VideoThumbnailProps {
@@ -33,7 +34,7 @@ export const VideoThumbnail = ({
 
       {/* Video duration box */}
       <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
-        {duration}
+        {formatDuration(duration)}
       </div>
     </div>
   );
