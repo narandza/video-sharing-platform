fb49ecb2b0c20582ff4c9b623fce97767adfde52
narandza <60319154+narandza@users.noreply.github.com>
2025-02-25 04:46:11 +0100

    error fix: minutes calculation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDuration = (duration: number) => {
   const seconds = Math.floor((duration % 60000) / 1000);
-  const minutes = Math.floor(duration / 1000);
+  const minutes = Math.floor(duration / 60000);
 
   return `${minutes.toString().padStart(2, "0")}:${seconds
     .toString()
