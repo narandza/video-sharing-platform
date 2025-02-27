846e2a6e6e43f9cb10aa21832495abad23c211cf
narandza <60319154+narandza@users.noreply.github.com>
2025-02-27 17:18:48 +0100

    add suspense to form section

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface FormSectionProps {
   videoId: string;
 }
 
 export const FormSection = ({ videoId }: FormSectionProps) => {
+  return (
+    <Suspense fallback={<FormSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <FormSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const FormSectionSkeleton = () => {
+  return <div className="">skeleton</div>;
+};
+
+const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
   return <div className="">{JSON.stringify(video)}</div>;
