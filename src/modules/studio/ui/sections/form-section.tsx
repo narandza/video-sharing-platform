82b077fab060f7c0f72c7acd247a747c4b77645d
narandza <60319154+narandza@users.noreply.github.com>
2025-02-27 17:30:46 +0100

    create form sectionn header

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -25,5 +26,19 @@ const FormSectionSkeleton = () => {
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
-  return <div className="">{JSON.stringify(video)}</div>;
+  return (
+    <div className="flex items-center justify-between mb-6">
+      <div className="">
+        <h1 className="text-2xl font-bold">Video details</h1>
+        <p className="text-xs text-muted-foreground">
+          Manage your video details
+        </p>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <Button type="submit" disabled={false}>
+          Save
+        </Button>
+      </div>
+    </div>
+  );
 };
