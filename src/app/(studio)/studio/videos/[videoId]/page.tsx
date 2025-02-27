a543669797b22ca3224d1cad948fa4480d5dec24
narandza <60319154+narandza@users.noreply.github.com>
2025-02-27 17:03:14 +0100

    prefetch video

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -1,4 +1,16 @@
-const Page = () => {
+import { trpc } from "@/trpc/server";
+
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  params: Promise<{ videoId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { videoId } = await params;
+
+  void trpc.studio.getOne({ id: videoId });
+
   return <div className="">video id</div>;
 };
 
