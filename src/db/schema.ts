ca69f020ddacf1ec789c99d39cc637008f46d8a3
narandza <60319154+narandza@users.noreply.github.com>
2025-02-28 18:09:55 +0100

    create update/insert/select videos schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,6 +9,12 @@ import {
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
+
 export const users = pgTable(
   "users",
   {
@@ -71,6 +77,12 @@ export const videos = pgTable("videos", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const videoInsertSchema = createInsertSchema(videos);
+
+export const videoUpdateSchema = createUpdateSchema(videos);
+
+export const videoSelectSchema = createSelectSchema(videos);
 
 export const videoRelations = relations(videos, ({ one }) => ({
   user: one(users, {
