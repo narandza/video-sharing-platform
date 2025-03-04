65c3f9274a2a5acd1e1de6e0d08114ae50cf3190
narandza <60319154+narandza@users.noreply.github.com>
2025-03-04 19:28:22 +0100

    add upload succes lgic

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "image.mux.com" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "image.mux.com" },
+      { protocol: "https", hostname: "utfs.io" },
+    ],
   },
 };
 
--- src/modules/studio/ui/components/thumbnail-upload-modal.tsx
+++ src/modules/studio/ui/components/thumbnail-upload-modal.tsx
@@ -1,5 +1,6 @@
 import { ResponsiveModal } from "@/components/responsive-modal";
 import { UploadDropzone } from "@/lib/uploadthing";
+import { trpc } from "@/trpc/client";
 
 interface ThumbnailUploadModalProps {
   videoId: string;
@@ -10,15 +11,27 @@ interface ThumbnailUploadModalProps {
 export const ThumbnailUploadModal = ({
   videoId,
   open,
-  onChange,
+  onOpenChange,
 }: ThumbnailUploadModalProps) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.studio.getMany.invalidate();
+    utils.studio.getOne.invalidate({ id: videoId });
+    onOpenChange(false);
+  };
+
   return (
     <ResponsiveModal
       title="Upload a thumbnail"
       open={open}
-      onOpenChange={onChange}
+      onOpenChange={onOpenChange}
     >
-      <UploadDropzone endpoint="imageUploader" />
+      <UploadDropzone
+        endpoint="thumbnailUploader"
+        input={{ videoId }}
+        onClientUploadComplete={onUploadComplete}
+      />
     </ResponsiveModal>
   );
 };
