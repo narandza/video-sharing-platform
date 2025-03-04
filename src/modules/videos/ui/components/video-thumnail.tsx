949b75681bb3ca4ac63229970d8a97069a862c71
narandza <60319154+narandza@users.noreply.github.com>
2025-03-04 07:40:36 +0100

    change statements

--- src/modules/videos/ui/components/video-thumnail.tsx
+++ src/modules/videos/ui/components/video-thumnail.tsx
@@ -21,14 +21,14 @@ export const VideoThumbnail = ({
       {/* Thumbnail wrapper  */}
       <div className="relative w-full overflow-hidden rounded-xl aspect-video">
         <Image
-          src={imageUrl ?? THUMBNAIL_FALLBACK}
+          src={imageUrl || THUMBNAIL_FALLBACK}
           alt={title}
           fill
           className="size-full object-cover group-hover:opacity-0"
         />
         <Image
           unoptimized={!!previewUrl}
-          src={previewUrl ?? THUMBNAIL_FALLBACK}
+          src={previewUrl || THUMBNAIL_FALLBACK}
           alt={title}
           fill
           className="size-full object-cover opacity-0 group-hover:opacity-100"
