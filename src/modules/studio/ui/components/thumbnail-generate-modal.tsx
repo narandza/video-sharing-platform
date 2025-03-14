dc641a96544268224c5528215768dbacefc96316
narandza <60319154+narandza@users.noreply.github.com>
2025-03-14 12:03:25 +0100

    qol to thumbnial generate modal

--- src/modules/studio/ui/components/thumbnail-generate-modal.tsx
+++ src/modules/studio/ui/components/thumbnail-generate-modal.tsx
@@ -44,6 +44,8 @@ export const ThumbnailGenerateModal = ({
       toast.success("Background job started", {
         description: "This may take some time",
       });
+      form.reset();
+      onOpenChange(false);
     },
     onError: () => {
       toast.error("Something went wrong");
@@ -88,7 +90,9 @@ export const ThumbnailGenerateModal = ({
             )}
           />
           <div className="flex justify-end">
-            <Button type="submit">Generate</Button>
+            <Button type="submit" disabled={generateThumbnail.isPending}>
+              Generate
+            </Button>
           </div>
         </form>
       </Form>
