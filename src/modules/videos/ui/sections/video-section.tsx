a35653aae6610627a5c41c599005cc65e6883d6f
narandza <60319154+narandza@users.noreply.github.com>
2025-03-17 22:55:03 +0100

    add video player

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
 
 interface VideoSectionProps {
   videoId: string;
@@ -21,5 +23,21 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
   const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
 
-  return <div className="">{JSON.stringify(video)}</div>;
+  return (
+    <>
+      <div
+        className={cn(
+          "aspect-video bg-black rounded-xl overflow-hidden relative",
+          video.muxStatus !== "ready" && "rounded-b-none"
+        )}
+      >
+        <VideoPlayer
+          autoPlay
+          onPlay={() => {}}
+          playbackId={video.muxPlaybackId}
+          thumbnailUrl={video.thumbnailUrl}
+        />
+      </div>
+    </>
+  );
 };
