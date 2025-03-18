34b61ba7e9aac5456f38761d2e273d2d04ec93cd
narandza <60319154+narandza@users.noreply.github.com>
2025-03-18 21:40:19 +0100

    create video owner component

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,4 +1,5 @@
 import { VideoGetOneOutput } from "../../types";
+import { VideoOwner } from "./video-owner";
 
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
@@ -8,7 +9,9 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
   return (
     <div className="flex flex-col gap-4 mt-4">
       <h1 className="text-xl font-semibold">{video.title}</h1>
-      <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4"></div>
+      <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
+        <VideoOwner user={video.user} videoId={video.id} />
+      </div>
     </div>
   );
 };
