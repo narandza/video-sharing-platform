23758b22a41847f1b9b2dc28c8df4622b47cc188
narandza <60319154+narandza@users.noreply.github.com>
2025-03-19 09:59:36 +0100

    create video reactions component ui

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,5 +1,6 @@
 import { VideoOwner } from "./video-owner";
 import { VideoGetOneOutput } from "../../types";
+import { VideoReactions } from "./video-reactions";
 
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
@@ -11,6 +12,12 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
       <h1 className="text-xl font-semibold">{video.title}</h1>
       <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
         <VideoOwner user={video.user} videoId={video.id} />
+        <div
+          className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2
+         -mb-2 sm:pb-0 sm:mb-0 gap-2"
+        >
+          <VideoReactions />
+        </div>
       </div>
     </div>
   );
