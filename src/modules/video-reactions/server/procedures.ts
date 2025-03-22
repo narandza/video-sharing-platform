2c77e9bf151ce845674ca6367726918ef5af1213
narandza <60319154+narandza@users.noreply.github.com>
2025-03-22 10:11:38 +0100

    create dislike procedure

--- src/modules/video-reactions/server/procedures.ts
+++ src/modules/video-reactions/server/procedures.ts
@@ -51,4 +51,49 @@ export const videoReactionsRouter = createTRPCRouter({
 
       return createdVideoReaction;
     }),
+  dislike: protectedProcedure
+    .input(z.object({ videoId: z.string().uuid() }))
+    .mutation(async ({ input, ctx }) => {
+      const { id: userId } = ctx.user;
+
+      const { videoId } = input;
+
+      const [existingVideoReactionDislike] = await db
+        .select()
+        .from(videoReactions)
+        .where(
+          and(
+            eq(videoReactions.videoId, videoId),
+            eq(videoReactions.userId, userId),
+            eq(videoReactions.type, "dislike")
+          )
+        );
+
+      if (existingVideoReactionDislike) {
+        const [deletedViewerReaction] = await db
+          .delete(videoReactions)
+          .where(
+            and(
+              eq(videoReactions.userId, userId),
+              eq(videoReactions.videoId, videoId)
+            )
+          )
+          .returning();
+
+        return deletedViewerReaction;
+      }
+
+      const [createdVideoReaction] = await db
+        .insert(videoReactions)
+        .values({ userId, videoId, type: "dislike" })
+        .onConflictDoUpdate({
+          target: [videoReactions.userId, videoReactions.videoId],
+          set: {
+            type: "dislike",
+          },
+        })
+        .returning();
+
+      return createdVideoReaction;
+    }),
 });
