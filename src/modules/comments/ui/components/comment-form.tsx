1bd6a9447efd5fc02458df359100510f371b560e
narandza <60319154+narandza@users.noreply.github.com>
2025-03-28 19:15:00 +0100

    Define comments form

--- src/modules/comments/ui/components/comment-form.tsx
+++ src/modules/comments/ui/components/comment-form.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { UserAvatar } from "@/components/user-avatar";
+import { commentInsertSchema } from "@/db/schema";
 import { useUser } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface CommentFormProps {
   videoId: string;
@@ -11,11 +15,19 @@ interface CommentFormProps {
 export const CommentForm = ({ videoId, onSuccess }: CommentFormProps) => {
   const { user } = useUser();
 
+  const form = useForm<z.infer<typeof commentInsertSchema>>({
+    resolver: zodResolver(commentInsertSchema.omit({ userId: true })),
+    defaultValues: {
+      videoId,
+      value: "",
+    },
+  });
+
   return (
     <form className="flex gap-4 group">
       <UserAvatar
         size="lg"
-        imageUrl={user?.imageUrl || "/user-placeholder.svg"}
+        imageUrl={user?.imageUrl || "/user- placeholder.svg"}
         name={user?.username || "User"}
       />
       <div className="flex-1">
