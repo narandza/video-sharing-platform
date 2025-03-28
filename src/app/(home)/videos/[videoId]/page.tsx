4759f248e5618eed23de4352b3c26bc9cafc0e4b
narandza <60319154+narandza@users.noreply.github.com>
2025-03-28 18:25:52 +0100

    prefetch comments

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -11,6 +11,7 @@ const Page = async ({ params }: PageProps) => {
   const { videoId } = await params;
 
   void trpc.videos.getOne.prefetch({ id: videoId });
+  void trpc.comments.getMany.prefetch({ videoId: videoId });
 
   return (
     <HydrateClient>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -6,11 +6,13 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedure";
 
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   categories: categoriesRouter,
   videos: videosRouter,
+  comments: commentsRouter,
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
   subscriptions: subscriptionsRouter,
