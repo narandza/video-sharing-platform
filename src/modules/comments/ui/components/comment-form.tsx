1bcad8af57594a764edf2514b8c32c29f4e973a3
narandza <60319154+narandza@users.noreply.github.com>
2025-03-30 21:25:39 +0200

    implement form ui

--- src/modules/comments/ui/components/comment-form.tsx
+++ src/modules/comments/ui/components/comment-form.tsx
@@ -9,6 +9,13 @@ import { UserAvatar } from "@/components/user-avatar";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 
 interface CommentFormProps {
   videoId: string;
@@ -52,23 +59,38 @@ export const CommentForm = ({ videoId, onSuccess }: CommentFormProps) => {
   };
 
   return (
-    <form className="flex gap-4 group">
-      <UserAvatar
-        size="lg"
-        imageUrl={user?.imageUrl || "/user- placeholder.svg"}
-        name={user?.username || "User"}
-      />
-      <div className="flex-1">
-        <Textarea
-          placeholder="Add a comment"
-          className="resize-none bg-transparent overflow-hidden min-h-0"
+    <Form onSubmit={form.handleSubmit(handleSubmit)} {...form}>
+      <form className="flex gap-4 group">
+        <UserAvatar
+          size="lg"
+          imageUrl={user?.imageUrl || "/user- placeholder.svg"}
+          name={user?.username || "User"}
         />
-        <div className="justify-end gap-2 mt-2 flex">
-          <Button type="submit" size="sm">
-            Comment
-          </Button>
+        <div className="flex-1">
+          <FormField
+            name="value"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="Add a comment"
+                    className="resize-none bg-transparent overflow-hidden min-h-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="justify-end gap-2 mt-2 flex">
+            <Button type="submit" size="sm">
+              Comment
+            </Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 };
