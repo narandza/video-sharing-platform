4fc2e2419b1003c550ff4c224e6c7cef27ea9bed
narandza <60319154+narandza@users.noreply.github.com>
2025-04-03 19:11:33 +0200

    create replies sub-query

--- src/modules/comments/server/procedure.ts
+++ src/modules/comments/server/procedure.ts
@@ -9,6 +9,7 @@ import {
   count,
   inArray,
   isNull,
+  isNotNull,
 } from "drizzle-orm";
 
 import {
@@ -113,6 +114,17 @@ export const commentsRouter = createTRPCRouter({
           .where(inArray(commentReactions.userId, userId ? [userId] : []))
       );
 
+      const replies = db.$with("replies").as(
+        db
+          .select({
+            parentId: comments.parentId,
+            count: count(comments.id).as("count"),
+          })
+          .from(comments)
+          .where(isNotNull(comments.parentId))
+          .groupBy(comments.parentId)
+      );
+
       const [totalData, data] = await Promise.all([
         db
           .select({
@@ -121,12 +133,12 @@ export const commentsRouter = createTRPCRouter({
           .from(comments)
           .where(eq(comments.videoId, videoId)),
         db
-          .with(viewerReactions)
+          .with(viewerReactions, replies)
           .select({
             ...getTableColumns(comments),
             user: users,
             viewerReaction: viewerReactions.type,
-            replyCount: db.$count(comments, eq(comments.id, comments.parentId)),
+            replyCount: replies.count,
             likeCount: db.$count(
               commentReactions,
               and(
@@ -160,6 +172,7 @@ export const commentsRouter = createTRPCRouter({
           )
           .innerJoin(users, eq(comments.userId, users.id))
           .leftJoin(viewerReactions, eq(comments.id, viewerReactions.commentId))
+          .leftJoin(replies, eq(comments.id, replies.parentId))
           .orderBy(desc(comments.updatedAt), desc(comments.id))
           .limit(limit + 1),
       ]);
