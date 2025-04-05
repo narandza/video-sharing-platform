9d7a96d7e2e879607f9cc0b964a4838ee054de97
narandza <60319154+narandza@users.noreply.github.com>
2025-04-05 12:15:34 +0200

    fix dropdown menu edge case

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -156,30 +156,30 @@ export const CommentItem = ({
             )}
           </div>
         </div>
-        <DropdownMenu modal={false}>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="size-8">
-              <MoreVerticalIcon />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {variant === "comment" && (
+        {comment.user.clerkId !== userId && variant === "reply" && (
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="size-8">
+                <MoreVerticalIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
               <DropdownMenuItem onClick={() => setIsReplyOpen(true)}>
                 <MessageSquareIcon className="size-4" />
                 Reply
               </DropdownMenuItem>
-            )}
 
-            {comment.user.clerkId === userId && (
-              <DropdownMenuItem
-                onClick={() => remove.mutate({ id: comment.id })}
-              >
-                <Trash2Icon className="size-4" />
-                Delete
-              </DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
+              {comment.user.clerkId === userId && (
+                <DropdownMenuItem
+                  onClick={() => remove.mutate({ id: comment.id })}
+                >
+                  <Trash2Icon className="size-4" />
+                  Delete
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
       {isReplyOpen && variant === "comment" && (
         <div className="mt-4 pl-14">
