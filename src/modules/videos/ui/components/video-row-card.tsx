3566db8707c56ff8a33f1d76cd10dc949825ac47
narandza <60319154+narandza@users.noreply.github.com>
2025-04-07 18:40:46 +0200

    create card&thumbnail variants; add output type

---
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -0,0 +1,25 @@
+import { cva } from "class-variance-authority";
+
+const videoRowCardVariants = cva("group flex min-w-0", {
+  variants: {
+    size: {
+      default: "gap-4",
+      compact: "gap-2",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+const thumbnailVariants = cva("relative flex-none", {
+  variants: {
+    size: {
+      default: "w-[38%]",
+      compact: "w-[168px]",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
