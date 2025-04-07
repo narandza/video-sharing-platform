09ec24341f8f6a51fffd9b10227c860ccd441ed9
narandza <60319154+narandza@users.noreply.github.com>
2025-04-07 18:16:11 +0200

    create get many suggestions procedure

---
+++ src/modules/suggestions/server/procedures.ts
@@ -0,0 +1,79 @@
+import { z } from "zod";
+import { and, desc, eq, lt, or } from "drizzle-orm";
+
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { TRPCError } from "@trpc/server";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+
+export const suggestionsRouter = createTRPCRouter({
+  getMany: baseProcedure
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input }) => {
+      const { videoId, cursor, limit } = input;
+
+      const [existingVideo] = await db
+        .select()
+        .from(videos)
+        .where(eq(videos.id, videoId));
+
+      if (!existingVideo) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const data = await db
+        .select()
+        .from(videos)
+        .where(
+          and(
+            existingVideo.categoryId
+              ? eq(videos.categoryId, existingVideo.categoryId)
+              : undefined,
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+
+      // Remove the last item if there is more data
+
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set the next cursor to the last item if there is more data
+
+      const lastItem = items[items.length - 1];
+
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+});
