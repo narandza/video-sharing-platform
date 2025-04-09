58b40e6a71f7120943cd564bf00d6e3e298a517b
narandza <60319154+narandza@users.noreply.github.com>
2025-04-09 16:48:44 +0200

    build suggestion section skeleton

--- src/modules/videos/ui/components/video-row-card.tsx
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -45,7 +45,9 @@ interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants> {
   onRemove?: () => void;
 }
 
-export const VideoRowCardSkeleton = ({ size }: VideoRowCardProps) => {
+export const VideoRowCardSkeleton = ({
+  size,
+}: VariantProps<typeof videoRowCardVariants>) => {
   return (
     <div className={videoRowCardVariants({ size })}>
       {/* Thumbnail skeleton */}
