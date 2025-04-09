43f0b5e4290ab6a56f9ac366f5301600fc41eb00
narandza <60319154+narandza@users.noreply.github.com>
2025-04-09 15:40:58 +0200

    implement inifnte scroll for sugestions

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -4,21 +4,27 @@ import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
 import { VideoRowCard } from "../components/video-row-card";
 import { VideoGridCard } from "../components/video-grid-card";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 
 interface SuggestionsSectionProps {
   videoId: string;
+  isManual?: boolean;
 }
 
-export const SuggestionsSection = ({ videoId }: SuggestionsSectionProps) => {
-  const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery(
-    {
-      videoId,
-      limit: DEFAULT_LIMIT,
-    },
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }
-  );
+export const SuggestionsSection = ({
+  videoId,
+  isManual,
+}: SuggestionsSectionProps) => {
+  const [suggestions, query] =
+    trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+      {
+        videoId,
+        limit: DEFAULT_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      }
+    );
 
   return (
     <>
@@ -36,6 +42,12 @@ export const SuggestionsSection = ({ videoId }: SuggestionsSectionProps) => {
           ))
         )}
       </div>
+      <InfiniteScroll
+        isManual={isManual}
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
     </>
   );
 };
