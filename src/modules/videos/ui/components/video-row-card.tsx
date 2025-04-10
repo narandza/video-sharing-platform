81e8cb0b32e5bc06503d0a85d8bdbf369b9575b8
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 19:34:47 +0200

    fix formating

--- src/modules/videos/ui/components/video-row-card.tsx
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -46,7 +46,7 @@ interface VideoRowCardProps extends VariantProps<typeof videoRowCardVariants> {
 }
 
 export const VideoRowCardSkeleton = ({
-  size,
+  size = "default",
 }: VariantProps<typeof videoRowCardVariants>) => {
   return (
     <div className={videoRowCardVariants({ size })}>
@@ -79,7 +79,11 @@ export const VideoRowCardSkeleton = ({
   );
 };
 
-export const VideoRowCard = ({ data, size, onRemove }: VideoRowCardProps) => {
+export const VideoRowCard = ({
+  data,
+  size = "default",
+  onRemove,
+}: VideoRowCardProps) => {
   const compactViews = useMemo(() => {
     return Intl.NumberFormat("en", {
       notation: "compact",
