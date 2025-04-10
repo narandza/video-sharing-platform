798c7fcd4833b096e165517c366eef341f7301b5
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 18:51:21 +0200

    build serarch view structure

--- src/app/(home)/search/page.tsx
+++ src/app/(home)/search/page.tsx
@@ -1,3 +1,6 @@
+import { SearchView } from "@/modules/search/ui/views/search-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
 export const dynamic = "force-dynamic";
 
 interface PageProps {
@@ -10,10 +13,12 @@ interface PageProps {
 const Page = async ({ searchParams }: PageProps) => {
   const { query, categoryId } = await searchParams;
 
+  void trpc.categories.getMany.prefetch();
+
   return (
-    <div className="">
-      Searching for {query} in category {categoryId}
-    </div>
+    <HydrateClient>
+      <SearchView query={query} categoryId={categoryId} />
+    </HydrateClient>
   );
 };
 
