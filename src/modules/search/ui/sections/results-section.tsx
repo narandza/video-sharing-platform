ee8acc6f7fa540b03f9efc3d0a4bff580b4042ab
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 19:28:48 +0200

    build result section ui

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -1,4 +1,7 @@
 import { DEFAULT_LIMIT } from "@/constants";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
+import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
 import { trpc } from "@/trpc/client";
 
 interface ResultsSectionProps {
@@ -7,6 +10,7 @@ interface ResultsSectionProps {
 }
 
 export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  const isMobile = useIsMobile();
   const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
     {
       query,
@@ -18,5 +22,25 @@ export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
     }
   );
 
-  return <div className="">{JSON.stringify(results)}</div>;
+  return (
+    <>
+      {isMobile ? (
+        <div className="flex flex-col gap-4 gap-y-10">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoGridCard key={video.id} data={video} />
+            ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {results.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <VideoRowCard key={video.id} data={video} />
+            ))}
+        </div>
+      )}
+    </>
+  );
 };
