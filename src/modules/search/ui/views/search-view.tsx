12acfa8acd974a675f4a612448538da1d35d9ab7
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 18:55:40 +0200

    add categories section

--- src/modules/search/ui/views/search-view.tsx
+++ src/modules/search/ui/views/search-view.tsx
@@ -1,3 +1,5 @@
+import { CategoriesSection } from "../sections/categories-section";
+
 interface SearchViewProps {
   query: string | undefined;
   categoryId: string | undefined;
@@ -5,6 +7,8 @@ interface SearchViewProps {
 
 export const SearchView = ({ query, categoryId }: SearchViewProps) => {
   return (
-    <div className="max-w-[1300px] mx-auto mb-10 flex flex-col gap-y-6 px-4 pt-2.5"></div>
+    <div className="max-w-[1300px] mx-auto mb-10 flex flex-col gap-y-6 px-4 pt-2.5">
+      <CategoriesSection categoryId={categoryId} />
+    </div>
   );
 };
