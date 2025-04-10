6791bcadd87a743a50db43c1681c02d2174438de
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 19:21:54 +0200

    create result section structure

---
+++ src/modules/search/ui/sections/results-section.tsx
@@ -0,0 +1,22 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+
+interface ResultsSectionProps {
+  query: string | undefined;
+  categoryId: string | undefined;
+}
+
+export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
+    {
+      query,
+      categoryId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return <div className="">{JSON.stringify(results)}</div>;
+};
