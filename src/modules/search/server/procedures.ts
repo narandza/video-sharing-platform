d67badda7d0dbc24ff604e0b802498ae866dce7b
narandza <60319154+narandza@users.noreply.github.com>
2025-04-10 19:32:32 +0200

    modifgy getMany search procedure

--- src/modules/search/server/procedures.ts
+++ src/modules/search/server/procedures.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
-import { and, desc, eq, ilike, lt, or } from "drizzle-orm";
+import { and, desc, eq, getTableColumns, ilike, lt, or } from "drizzle-orm";
 
 import { db } from "@/db";
-import { videos } from "@/db/schema";
+import { users, videoReactions, videoViews, videos } from "@/db/schema";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const searchRouter = createTRPCRouter({
@@ -24,8 +24,27 @@ export const searchRouter = createTRPCRouter({
       const { cursor, limit, query, categoryId } = input;
 
       const data = await db
-        .select()
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like")
+            )
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike")
+            )
+          ),
+        })
         .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
         .where(
           and(
             ilike(videos.title, `%${query}%`),
