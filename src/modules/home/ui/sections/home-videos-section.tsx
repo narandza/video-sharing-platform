40e4ea3485a4476255589d293e8a0878ad6651fe
narandza <60319154+narandza@users.noreply.github.com>
2025-04-14 17:24:37 +0200

    fetch videos; create grid structure

--- src/modules/home/ui/sections/home-videos-section.tsx
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
+import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -20,5 +25,25 @@ const HomeVideosSectionSkeleton = () => {
 };
 
 const HomeVideosSectionSuspense = ({ categoryId }: HomeVideosSectionProps) => {
-  return <div className="">home videos section</div>;
+  const [videos, query] = trpc.videos.getMany.useSuspenseInfiniteQuery(
+    {
+      categoryId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div className="">
+      <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+    </div>
+  );
 };
