ee8013dbbefdbef650b10950710c4e135d3ff2c0
narandza <60319154+narandza@users.noreply.github.com>
2025-04-15 17:57:17 +0200

    build trending videos section skeleton

--- src/modules/home/ui/sections/trending-videos-section.tsx
+++ src/modules/home/ui/sections/trending-videos-section.tsx
@@ -2,7 +2,10 @@
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
-import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -18,7 +21,13 @@ export const TrendingVideosSection = () => {
 };
 
 const TrendingVideosSectionSkeleton = () => {
-  return <div className="">loading</div>;
+  return (
+    <div>
+      {Array.from({ length: 18 }).map((_, index) => (
+        <VideoGridCardSkeleton key={index} />
+      ))}
+    </div>
+  );
 };
 
 const TrendingVideosSectionSuspense = () => {
@@ -32,7 +41,7 @@ const TrendingVideosSectionSuspense = () => {
   );
 
   return (
-    <div className="">
+    <div>
       <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
         {videos.pages
           .flatMap((page) => page.items)
