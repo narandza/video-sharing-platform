0471e9413ddd906206a42f6416fbcfeed8df1e1a
narandza <60319154+narandza@users.noreply.github.com>
2025-04-15 17:54:21 +0200

    build trending videos section ui

--- src/modules/home/ui/sections/trending-videos-section.tsx
+++ src/modules/home/ui/sections/trending-videos-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { DEFAULT_LIMIT } from "@/constants";
+import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
@@ -29,5 +30,15 @@ const TrendingVideosSectionSuspense = () => {
     }
   );
 
-  return <div className="">trending videos section</div>;
+  return (
+    <div className="">
+      <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+    </div>
+  );
 };
