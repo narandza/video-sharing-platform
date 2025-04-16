5407c71b423ebe05b3af5abe6e468441a25d173d
narandza <60319154+narandza@users.noreply.github.com>
2025-04-16 17:37:24 +0200

    prefetch playlists

---
+++ src/app/(home)/playlists/history/page.tsx
@@ -0,0 +1,9 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+const Page = async () => {
+  void trpc.playlists.getHistory.prefetchInfinite({ limit: DEFAULT_LIMIT });
+  return <HydrateClient>{/* TODO: History View */}</HydrateClient>;
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -10,6 +10,7 @@ import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedures";
+import { playlistsRouter } from "@/modules/playlists/server/procedure";
 
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
@@ -22,6 +23,7 @@ export const appRouter = createTRPCRouter({
   subscriptions: subscriptionsRouter,
   videoReactions: videoReactionsRouter,
   commentReactions: commentReactionsRouter,
+  playlists: playlistsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
