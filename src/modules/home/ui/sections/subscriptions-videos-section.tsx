08e167f11a0f84a4ccf45192103d209bf9586f6d
narandza <60319154+narandza@users.noreply.github.com>
2025-04-16 16:45:08 +0200

    build subsciptions videos section module structure

---
+++ src/modules/home/ui/sections/subscriptions-videos-section.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+
+export const SubscriptionsVideosSection = () => {
+  return (
+    <Suspense fallback={<SubscriptionsVideosSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <SubscriptionsVideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const SubscriptionsVideosSectionSkeleton = () => {
+  return <div className="">loading</div>;
+};
+
+const SubscriptionsVideosSectionSuspense = () => {
+  const [videos, query] = trpc.videos.getManyTrending.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div>
+      <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
