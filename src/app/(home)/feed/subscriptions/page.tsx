4ba4061d12d2b25c4ec3ec8fe144260380a72b64
narandza <60319154+narandza@users.noreply.github.com>
2025-04-16 16:39:56 +0200

    build subscriptions page

---
+++ src/app/(home)/feed/subscriptions/page.tsx
@@ -0,0 +1,19 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { TrendingView } from "@/modules/home/ui/views/trending-view";
+
+export const dynamic = "force-dynamic";
+
+const Page = async () => {
+  void trpc.videos.getManySubscribed.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <TrendingView />
+    </HydrateClient>
+  );
+};
+
+export default Page;
