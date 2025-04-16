0818ddb288c32d3271b91064134ef8a00d9c38c8
narandza <60319154+narandza@users.noreply.github.com>
2025-04-16 17:33:35 +0200

    build get history playlist procedure

---
+++ src/modules/playlists/server/procedure.ts
@@ -0,0 +1,100 @@
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
+import { z } from "zod";
+
+import { users, videoReactions, videoViews, videos } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { db } from "@/db";
+
+export const playlistsRouter = createTRPCRouter({
+  getHistory: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            viewedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { id: userId } = ctx.user;
+      const { cursor, limit } = input;
+
+      const viewerVideoViews = db.$with("viewer_video_views").as(
+        db
+          .select({
+            videoId: videoViews.videoId,
+            viewedAt: videoViews.updatedAt,
+          })
+          .from(videoViews)
+          .where(eq(videoViews.userId, userId))
+      );
+
+      const data = await db
+        .with(viewerVideoViews)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewedAt: viewerVideoViews.viewedAt,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like")
+            )
+          ),
+          dislikeCount: db.$count(
+            videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike")
+            )
+          ),
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(viewerVideoViews, eq(viewerVideoViews.videoId, videos.id))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+
+            cursor
+              ? or(
+                  lt(viewerVideoViews.viewedAt, cursor.viewedAt),
+                  and(
+                    eq(viewerVideoViews.viewedAt, cursor.viewedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(viewerVideoViews.viewedAt), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+
+      // Remove the last item if there is more data
+
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set the next cursor to the last item if there is more data
+
+      const lastItem = items[items.length - 1];
+
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            viewedAt: lastItem.viewedAt,
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+});
