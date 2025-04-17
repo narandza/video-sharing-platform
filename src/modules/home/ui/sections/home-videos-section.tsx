697982cf81d35a681abf9d27753e0bb34b976ecd
narandza <60319154+narandza@users.noreply.github.com>
2025-04-17 18:56:40 +0200

    fix ui

--- src/modules/home/ui/sections/home-videos-section.tsx
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -27,7 +27,7 @@ export const HomeVideosSection = ({ categoryId }: HomeVideosSectionProps) => {
 
 const HomeVideosSectionSkeleton = () => {
   return (
-    <div className="">
+    <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
       {Array.from({ length: 18 }).map((_, index) => (
         <VideoGridCardSkeleton key={index} />
       ))}
--- src/modules/playlists/ui/sections/history-videos-section.tsx
+++ src/modules/playlists/ui/sections/history-videos-section.tsx
@@ -10,6 +10,10 @@ import {
   VideoGridCardSkeleton,
 } from "@/modules/videos/ui/components/video-grid-card";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
 
 export const HistoryVideosSection = () => {
   return (
@@ -24,9 +28,16 @@ export const HistoryVideosSection = () => {
 const HistoryVideosSectionSkeleton = () => {
   return (
     <div>
-      {Array.from({ length: 18 }).map((_, index) => (
-        <VideoGridCardSkeleton key={index} />
-      ))}
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4  md:flex">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -43,11 +54,18 @@ const HistoryVideosSectionSuspense = () => {
 
   return (
     <div>
-      <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5  [@media(min-width:2200px)]:grid-cols-6">
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
         {videos.pages
           .flatMap((page) => page.items)
           .map((video) => (
             <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+      <div className="hidden flex-col gap-4  md:flex">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoRowCard key={video.id} data={video} size="compact" />
           ))}
       </div>
       <InfiniteScroll
