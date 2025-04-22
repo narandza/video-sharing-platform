19e8371ca8083a0420f013940743480abb0964a9
narandza <60319154+narandza@users.noreply.github.com>
2025-04-22 13:40:50 +0200

    build playlists section struture

---
+++ src/modules/playlists/ui/sections/playlists-section.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+
+export const PlaylistsSection = () => {
+  return (
+    <Suspense fallback={<PlaylistsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <PlaylistsSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const PlaylistsSectionSkeleton = () => {
+  return <>Loading...</>;
+};
+
+const PlaylistsSectionSuspense = () => {
+  const [videos, query] = trpc.playlists.getHistory.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return <>section</>;
+};
--- src/modules/playlists/ui/views/playlists-view.tsx
+++ src/modules/playlists/ui/views/playlists-view.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { PlaylistCreateModal } from "../components/playlist-create-modal";
 import { useState } from "react";
+import { PlaylistsSection } from "../sections/playlists-section";
 
 export const PlaylistsView = () => {
   const [createModalOpen, setCreateModalOpen] = useState(false);
@@ -29,6 +30,7 @@ export const PlaylistsView = () => {
           <PlusIcon />
         </Button>
       </div>
+      <PlaylistsSection />
     </div>
   );
 };
