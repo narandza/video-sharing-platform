9dc683720396a0720a9e7ca017ea062546fd5af7
narandza <60319154+narandza@users.noreply.github.com>
2025-04-25 12:53:59 +0200

    build playlist header section ui

--- src/modules/playlists/ui/sections/playlist-header-section.tsx
+++ src/modules/playlists/ui/sections/playlist-header-section.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Trash2Icon } from "lucide-react";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -20,5 +22,17 @@ export const PlaylistHeaderSection = ({
 const PlaylistHeaderSectionSuspense = ({
   playlistId,
 }: PlaylistHeaderSectionProps) => {
-  return <div className="">TODO: Build UI</div>;
+  return (
+    <div className="flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-bold">Playlist name</h1>
+        <p className="text-xs text-muted-foreground">
+          Videos from the playlist
+        </p>
+      </div>
+      <Button variant="outline" size="icon" className="rounded-full">
+        <Trash2Icon />
+      </Button>
+    </div>
+  );
 };
