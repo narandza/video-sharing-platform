b977d47f9afa15d3ec0eee15f8ed28a44053dd46
narandza <60319154+narandza@users.noreply.github.com>
2025-04-25 12:59:06 +0200

    build get one playlist procedure

--- src/modules/playlists/server/procedure.ts
+++ src/modules/playlists/server/procedure.ts
@@ -14,6 +14,28 @@ import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const playlistsRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { id } = input;
+      const { id: userId } = ctx.user;
+
+      const [existingPlaylist] = await db
+        .select()
+        .from(playlists)
+        .where(and(eq(playlists.id, id), eq(playlists.userId, userId)));
+
+      if (!existingPlaylist) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return existingPlaylist;
+    }),
+
   getVideos: protectedProcedure
     .input(
       z.object({
