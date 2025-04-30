7fe0a9f9cb7301de27a4a2268d83f75b8b4dd8cb
narandza <60319154+narandza@users.noreply.github.com>
2025-04-30 10:23:48 +0200

    add edit button

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -1,11 +1,16 @@
 import { cn } from "@/lib/utils";
 import { UserGetOneOutput } from "../../types";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Edit2Icon } from "lucide-react";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
 }
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
+  const { userId } = useAuth();
+
   return (
     <div className="relative group">
       <div
@@ -18,7 +23,16 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
             ? `url(${user.bannerUrl})`
             : undefined,
         }}
-      ></div>
+      >
+        {user.clerkId === userId && (
+          <Button
+            size="icon"
+            className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+          >
+            <Edit2Icon className="size-4 text-white" />
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
