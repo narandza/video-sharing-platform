090ee32b279e8d9c933877edc59d0026ec329171
narandza <60319154+narandza@users.noreply.github.com>
2025-04-30 10:25:58 +0200

    build user banner skeleton

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -3,10 +3,15 @@ import { UserGetOneOutput } from "../../types";
 import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
 }
+
+export const UserPageBannerSkeleton = () => {
+  return <Skeleton className="w-full max-h-[200px] h-[15vh] md:h-[25vh] " />;
+};
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth();
