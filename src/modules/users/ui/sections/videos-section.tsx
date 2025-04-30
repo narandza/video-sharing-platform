7fdd91fe0c153d7169a9f0da140b57717ce445d3
narandza <60319154+narandza@users.noreply.github.com>
2025-04-30 11:27:39 +0200

    build video section skeleton

--- src/modules/users/ui/sections/videos-section.tsx
+++ src/modules/users/ui/sections/videos-section.tsx
@@ -5,7 +5,10 @@ import { ErrorBoundary } from "react-error-boundary";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
-import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
 import { InfiniteScroll } from "@/components/infinite-scroll";
 
 interface VideosSectionProps {
@@ -23,7 +26,13 @@ export const VideosSection = ({ userId }: VideosSectionProps) => {
 };
 
 const VideosSectionSkeleton = () => {
-  return <div className=""></div>;
+  return (
+    <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+      {Array.from({ length: 18 }).map((_, index) => (
+        <VideoGridCardSkeleton key={index} />
+      ))}
+    </div>
+  );
 };
 
 const VideosSectionSuspense = ({ userId }: VideosSectionProps) => {
