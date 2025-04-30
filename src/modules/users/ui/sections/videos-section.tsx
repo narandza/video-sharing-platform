6fd1607994071d031a199c1768c25a5304a4a59e
narandza <60319154+narandza@users.noreply.github.com>
2025-04-30 11:26:14 +0200

    build videos section ui

--- src/modules/users/ui/sections/videos-section.tsx
+++ src/modules/users/ui/sections/videos-section.tsx
@@ -5,6 +5,7 @@ import { ErrorBoundary } from "react-error-boundary";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
+import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
 
 interface VideosSectionProps {
   userId: string;
@@ -21,7 +22,7 @@ export const VideosSection = ({ userId }: VideosSectionProps) => {
 };
 
 const VideosSectionSkeleton = () => {
-  return <div className="">UI</div>;
+  return <div className="">Loading</div>;
 };
 
 const VideosSectionSuspense = ({ userId }: VideosSectionProps) => {
@@ -35,5 +36,15 @@ const VideosSectionSuspense = ({ userId }: VideosSectionProps) => {
     }
   );
 
-  return <>UI</>;
+  return (
+    <>
+      <div className="gap-4 gap-y-10 grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+    </>
+  );
 };
