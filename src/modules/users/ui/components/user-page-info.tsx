6de5bf4573cd0e64f797321f11ef26805b1a0bb2
narandza <60319154+narandza@users.noreply.github.com>
2025-05-01 11:33:13 +0200

    build user page info mobile layout skeleton

--- src/modules/users/ui/components/user-page-info.tsx
+++ src/modules/users/ui/components/user-page-info.tsx
@@ -6,10 +6,29 @@ import Link from "next/link";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
 import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface UserPageInfoProps {
   user: UserGetOneOutput;
 }
+
+export const UserPageInfoSkeleton = () => {
+  return (
+    <div className="py-6">
+      {/* Mobile layout */}
+      <div className="flex flex-col md:hidden">
+        <div className="flex items-center gap-3">
+          <Skeleton className="h-[60px] w-[60px] rounded-full" />
+          <div className="flex-1 min-w-0">
+            <Skeleton className="h-6 w-32" />
+            <Skeleton className="h-4 w-48 mt-1" />
+          </div>
+        </div>
+        <Skeleton className="h-10 w-full mt-3 rounded-full" />
+      </div>
+    </div>
+  );
+};
 
 export const UserPageInfo = ({ user }: UserPageInfoProps) => {
   const { userId, isLoaded } = useAuth();
