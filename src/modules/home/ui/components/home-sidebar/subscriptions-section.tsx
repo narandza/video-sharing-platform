4b81146650eac85c671e87f521097321190e0430
narandza <60319154+narandza@users.noreply.github.com>
2025-05-02 10:57:42 +0200

    build subscriptions section on the home sidebar

---
+++ src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { trpc } from "@/trpc/client";
+import { UserAvatar } from "@/components/user-avatar";
+import { DEFAULT_LIMIT } from "@/constants";
+
+export const SubscriptionsSection = () => {
+  const pathname = usePathname();
+
+  const { data } = trpc.subscriptions.getMany.useInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <SidebarGroup className="">
+      <SidebarGroupLabel>Subscriptions</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {data?.pages
+            .flatMap((page) => page.items)
+            .map((subscription) => (
+              <SidebarMenuItem
+                key={`${subscription.creatorId}-${subscription.viewerId}`}
+              >
+                <SidebarMenuButton
+                  tooltip={subscription.user.name}
+                  asChild
+                  isActive={pathname === `/users/${subscription.user.id}`}
+                >
+                  <Link
+                    href={`/users/${subscription.user.id}`}
+                    className="flex items-center gap-4"
+                  >
+                    <UserAvatar
+                      size="xs"
+                      imageUrl={subscription.user.imageUrl}
+                      name={subscription.user.name}
+                    />
+                    <span className="text-sm">{subscription.user.name}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
