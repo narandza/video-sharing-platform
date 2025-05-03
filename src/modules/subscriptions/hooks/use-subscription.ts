4a2b54ea4256db6ca6a91dd80715c4daa6c698d7
narandza <60319154+narandza@users.noreply.github.com>
2025-05-03 10:21:17 +0200

    implelemnt subsctiptions section +invalidate

--- src/modules/subscriptions/hooks/use-subscription.ts
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -21,7 +21,7 @@ export const useSubscription = ({
   const subscribe = trpc.subscriptions.create.useMutation({
     onSuccess: () => {
       toast.success("Subscribed");
-
+      utils.subscriptions.getMany.invalidate();
       utils.videos.getManySubscribed.invalidate();
       utils.users.getOne.invalidate({ id: userId });
 
@@ -41,7 +41,7 @@ export const useSubscription = ({
   const unsubscribe = trpc.subscriptions.remove.useMutation({
     onSuccess: () => {
       toast.success("Unsubscribed");
-
+      utils.subscriptions.getMany.invalidate();
       utils.videos.getManySubscribed.invalidate();
       utils.users.getOne.invalidate({ id: userId });
 
