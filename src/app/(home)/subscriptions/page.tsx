a1e29bfb484e1441cdaf60e59f54a39614afff45
narandza <60319154+narandza@users.noreply.github.com>
2025-05-05 22:31:46 +0200

    build subscriptions page

---
+++ src/app/(home)/subscriptions/page.tsx
@@ -0,0 +1,12 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+const Page = async () => {
+  void trpc.subscriptions.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+  return <HydrateClient>
+    <SubscriptionsView />
+  </HydrateClient>
+
+export default Page;
