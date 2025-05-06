1245f50ad4aa4cdbd23eb4e3bfcf8ad127d03ddc
narandza <60319154+narandza@users.noreply.github.com>
2025-05-06 18:09:11 +0200

    build subscription item skeleton

--- src/modules/subscriptions/ui/components/subscription-item.tsx
+++ src/modules/subscriptions/ui/components/subscription-item.tsx
@@ -1,6 +1,7 @@
 import { UserAvatar } from "@/components/user-avatar";
 
 import { SubscriptionButton } from "./subscription-button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface SubscriptionItemProps {
   name: string;
@@ -9,6 +10,24 @@ interface SubscriptionItemProps {
   onUnsubscribe: () => void;
   disabled: boolean;
 }
+
+export const SubscriptionItemSkeleton = () => {
+  return (
+    <div className="flex items-start gap-4">
+      <Skeleton className="size-10 rounded-full" />
+      <div className="flex-1">
+        <div className="flex items-center justify-between">
+          <div>
+            <Skeleton className="h-4 w-24 " />
+            <Skeleton className="mt-1 w-20 h-3" />
+          </div>
+
+          <Skeleton className="h-8 w-20" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const SubscriptionItem = ({
   name,
