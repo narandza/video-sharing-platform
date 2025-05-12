3a21c6632c6a7f75653c4e15a8374f98df842eb8
narandza <60319154+narandza@users.noreply.github.com>
2025-05-12 18:25:12 +0200

    generate meta data for liked page

--- src/app/(home)/playlists/liked/page.tsx
+++ src/app/(home)/playlists/liked/page.tsx
@@ -1,8 +1,16 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { LikedView } from "@/modules/playlists/ui/views/liked-view";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Liked Videos - New Tube",
+    description: "See all the videos you liked",
+  };
+};
 
 const Page = async () => {
   void trpc.playlists.getLiked.prefetchInfinite({ limit: DEFAULT_LIMIT });
