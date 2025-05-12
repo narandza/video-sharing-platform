ef9785590862f1c94fcad54d17b6dcaf44ede6e8
narandza <60319154+narandza@users.noreply.github.com>
2025-05-12 18:45:47 +0200

    build metadata for history page

--- src/app/(home)/playlists/history/page.tsx
+++ src/app/(home)/playlists/history/page.tsx
@@ -1,8 +1,16 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { HistoryView } from "@/modules/playlists/ui/views/history-view";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "History - New Tube",
+    description: "See all videos you watched.",
+  };
+};
 
 const Page = async () => {
   void trpc.playlists.getHistory.prefetchInfinite({ limit: DEFAULT_LIMIT });
