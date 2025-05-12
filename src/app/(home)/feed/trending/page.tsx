de021445b7393a1519041a8bf90d92e1684f4efa
narandza <60319154+narandza@users.noreply.github.com>
2025-05-12 18:44:36 +0200

    generate metadata for trending page

--- src/app/(home)/feed/trending/page.tsx
+++ src/app/(home)/feed/trending/page.tsx
@@ -1,8 +1,16 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { TrendingView } from "@/modules/home/ui/views/trending-view";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Trending - New Tube",
+    description: "See all the videos that are trending right now.",
+  };
+};
 
 const Page = async () => {
   void trpc.videos.getManyTrending.prefetchInfinite({
