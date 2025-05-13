9e86bbb1949749bd1c4154ad995c504dab95dbfd
narandza <60319154+narandza@users.noreply.github.com>
2025-05-13 16:35:56 +0200

    generate playlists page meta data

--- src/app/(home)/playlists/page.tsx
+++ src/app/(home)/playlists/page.tsx
@@ -1,8 +1,16 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { PlaylistsView } from "@/modules/playlists/ui/views/playlists-view";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Playlists - New Tube",
+    description: "See all the playlist you have created",
+  };
+};
 
 const Page = async () => {
   void trpc.playlists.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
