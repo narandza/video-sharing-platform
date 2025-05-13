9fb3d4813601cab20db779e8ca91a47e3fdae329
narandza <60319154+narandza@users.noreply.github.com>
2025-05-13 16:44:37 +0200

    generate sign in page metadata

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Sign In - New Tube",
+    description: "Sign in to New Tube",
+  };
+};
 
 export default function Page() {
   return <SignIn />;
