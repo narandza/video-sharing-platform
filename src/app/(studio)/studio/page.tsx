4892ca43454337273c983efc3a0f45713a9704a0
narandza <60319154+narandza@users.noreply.github.com>
2025-05-13 16:40:36 +0200

    generate studio page metadata

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,8 +1,16 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { StudioView } from "@/modules/studio/ui/views/studio-view";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Channel Content - New Tube",
+    description: "Content you created",
+  };
+};
 
 const Page = async () => {
   void trpc.studio.getMany.prefetchInfinite({
