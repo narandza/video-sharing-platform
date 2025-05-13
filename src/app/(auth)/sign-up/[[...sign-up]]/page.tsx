d9170f2e91eb7e7f95e0aa52c1042c2a4ecaf525
narandza <60319154+narandza@users.noreply.github.com>
2025-05-13 16:45:38 +0200

    generate sing up page metadata

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Sign Up - New Tube",
+    description: "Create an account",
+  };
+};
 
 export default function Page() {
   return <SignUp />;
