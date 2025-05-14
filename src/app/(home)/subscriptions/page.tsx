0c572c684ce63f7011387bcca6cb2e6056f6c685
narandza <60319154+narandza@users.noreply.github.com>
2025-05-14 18:35:42 +0200

    generate metadata for subscriptions page

--- src/app/(home)/subscriptions/page.tsx
+++ src/app/(home)/subscriptions/page.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from "next";
+
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { SubscriptionsView } from "@/modules/subscriptions/ui/views/subscriptions-view";
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Subscriptions - New Tube",
+    description: "See all the channels and creators you're subscribed to.",
+  };
+};
 
 const Page = async () => {
   void trpc.subscriptions.getMany.prefetchInfinite({
